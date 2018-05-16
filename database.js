dba1f9030f223799e58de517329f21e7d68822c2
Jan Belterman <jjabelte@avans.nl>
2018-05-16 17:09:54 +0200

    Improved tests

--- database.js
+++ database.js
@@ -2,10 +2,10 @@ const mySql = require('mysql');
 
 // Database connecting
 let database = mySql.createConnection( {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: 'localhost',
+    user: 'studentenhuis_user',
+    password: 'secret',
+    database: 'studentenhuis',
     insecureAuth: true
 });
 database.connect( (error) => {
