7347379c3d878cdb7c4b75d0e04b7c1dd7b936b4
Floris Botermans <fboterma@avans.nl>
2018-05-16 17:13:06 +0200

    Fixed deelnemer

--- routes/deelnemer.js
+++ routes/deelnemer.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const auth = require('../middleware/auth');
 const database = require('../database');
-const { validate } = require('../models/deelnemer');
 
 // Get router
 const router = express.Router();
@@ -15,17 +14,16 @@ router.post('/:studentenhuisId/maaltijd/:maaltijdId/deelnemers', auth, (req, res
         MaaltijdID: req.params.maaltijdId
     };
 
-    const { error } = validate(deelnemer);
-    if (error) return res.status(404).send(error.details[0].message);
-
-    database.query(`SELECT * FROM deelnemers WHERE UserID = '${deelnemer.UserID}' AND MaaltijdID = '${deelnemer.MaaltijdID}'`, (error, result, fields) => {
+    database.query(`SELECT * FROM deelnemers WHERE UserID = '${deelnemer.UserID}' AND StudentenhuisID = '${deelnemer.StudentenhuisID}' AND MaaltijdID = '${deelnemer.MaaltijdID}'`, (error, result, fields) => {
         console.log(result);
         if (result.length > 0) return res.status(409).send('Conflict (Gebruiker is al aangemeld)');
 
         database.query('INSERT INTO deelnemers SET ?', deelnemer, (error, result, fields) => {
-            return res.status(200).json(result);
+            if (error) return res.status(404).send('Niet gevonden (huisId of maaltijdId bestaat niet)');
+            res.status(200).json(result);
         });
     });
+
 });
 
 router.get('/:studentenhuisId/maaltijd/:maaltijdId/deelnemers', auth, (req, res) => {
@@ -36,10 +34,8 @@ router.get('/:studentenhuisId/maaltijd/:maaltijdId/deelnemers', auth, (req, res)
         MaaltijdID: req.params.maaltijdId
     };
 
-    const { error } = validate(deelnemer);
-    if (error) return res.status(404).send(error.details[0].message);
-
     database.query(`SELECT * FROM deelnemers WHERE StudentenhuisID = '${deelnemer.StudentenhuisID}' AND MaaltijdID = '${deelnemer.MaaltijdID}'`, (error, result, fields) => {
+        if (result.length === 0) return res.status(404).send('Niet gevonden (huisId of maaltijdId bestaat niet)');
         res.status(200).json(result);
     });
 });
@@ -52,11 +48,9 @@ router.delete('/:studentenhuisId/maaltijd/:maaltijdId/deelnemers', auth, (req, r
         MaaltijdID: req.params.maaltijdId
     };
 
-    const { error } = validate(deelnemer);
-    if (error) return res.status(404).send(error.details[0].message);
-
     database.query(`SELECT * FROM deelnemers WHERE StudentenhuisID = '${deelnemer.StudentenhuisID}' AND MaaltijdID = '${deelnemer.MaaltijdID}'`, (error, result, fields) => {
-        if (result.length < 1 || !(deelnemer.UserID.toString() === result[0].UserID.toString())) return res.status(409).send('Conflict (Gebruiker mag deze data niet wijzigen)');
+        if (result.length === 0) return res.status(404).send('Niet gevonden (huisId of maaltijdId bestaat niet)');
+        if (!(deelnemer.UserID.toString() === result[0].UserID.toString())) return res.status(409).send('Conflict (Gebruiker mag deze data niet wijzigen)');
         database.query(`DELETE FROM deelnemers WHERE StudentenhuisID = '${deelnemer.StudentenhuisID}' AND MaaltijdID = '${deelnemer.MaaltijdID}'`, (err, result) => {
 
             res.status(200).send(`Deelnemer met id: ${deelnemer.UserID} met succes verwijderd`);
