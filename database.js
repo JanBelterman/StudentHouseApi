68f7132dbd5bfba9fe9ca24ac2e3b187faabef4a
Jan Belterman <jjabelte@avans.nl>
2018-05-16 21:59:24 +0200

    Fixed database name in env's

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ let database = mySql.createConnection( {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE,
+    database: process.env.DB_DATABASE,
     insecureAuth: true
 });
 database.connect( (error) => {
